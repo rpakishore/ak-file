ae7adcf455682cd8ba54ae4dd42aa5238a12c030
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2023-09-01 11:55:21 -0700

    refactored

--- ak_file/sanitize.py
+++ ak_file/sanitize.py
@@ -1,60 +1,69 @@
 import unicodedata
 import re
+from pathlib import Path
 
-blacklist = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|", "\0"]
-
-# Reserved words on Windows
-reserved = [
-    "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5",
-    "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5",
-    "LPT6", "LPT7", "LPT8", "LPT9",
-]  
+MAX_FILENAME_LENGTH = 255
+MAX_EXTENSION_LENGTH = 254
+MIN_ASCII_VALUE = 32
 
 def sanitize(filename:str) -> str:
     """Return a fairly safe version of the filename.
-
-    We don't limit ourselves to ascii, because we want to keep municipality
-    names, etc, but we do want to get rid of anything potentially harmful,
-    and make sure we do not exceed Windows filename length limits.
-    Hence a less safe blacklist, rather than a whitelist.
     """
+    filepath: Path = Path(filename)
+    filedir: Path = filename.parent
+    filename = filepath.name
     
-    filename = "".join(c for c in filename if c not in blacklist)
-    # Remove all charcters below code point 32
-    filename = "".join(c for c in filename if 31 < ord(c))
+    filename = remove_blacklisted_characters(filename)
+    filename = remove_chars_below_code_point(filename, MIN_ASCII_VALUE)
+    filename = normalize_filename(filename)
+    filename, ext = handle_extension(filename)
+    filename = handle_reserved(filename)
+    filename = handle_max_length(filename, ext, 
+                                    MAX_FILENAME_LENGTH, MAX_EXTENSION_LENGTH)
+    filepath = filedir / filename
+    return str(filepath)
+
+def remove_blacklisted_characters(filename: str) -> str:
+    """Remove blacklisted characters from the filename."""
+    blacklist = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|", "\0"]
+    return "".join(c for c in filename if c not in blacklist)
+
+def remove_chars_below_code_point(filename: str, code_point: int) -> str:
+    """Remove characters from the filename below a certain ASCII code point."""
+    return "".join(c for c in filename if ord(c) > code_point)
+
+def normalize_filename(filename: str) -> str:
+    """Normalize the filename and remove trailing/leading spaces and periods."""
     filename = unicodedata.normalize("NFKD", filename)
-    filename = filename.rstrip(". ")  # Windows does not allow these at end
-    filename = filename.strip()
-    if all([x == "." for x in filename]):
-        filename = "__" + filename
+    filename = filename.rstrip(". ").strip()
+    return "__" + filename if all([x == "." for x in filename]) else filename
 
+def handle_extension(filename: str) -> tuple[str, str]:
+    """Handle the extension of the filename."""
     parts = re.split(r"/|\\", filename)[-1].split(".")
-    ext=''
-    if len(parts) > 1:
-        ext = "." + parts.pop()
-        filename = filename[:-len(ext)]
-    if filename in reserved:
-        filename = "__" + filename
-    filename = filename + ext
+    ext = "." + parts.pop() if len(parts) > 1 else ""
+    filename = filename[:-len(ext)] if ext else filename
+    return filename, ext
 
-    if len(filename) == 0:
-        filename = "__"
-    if len(filename) > 255:
-        parts = re.split(r"/|\\", filename)[-1].split(".")
-        if len(parts) > 1:
-            ext = "." + parts.pop()
-            filename = filename[:-len(ext)]
-        else:
-            ext = ""
-        if filename == "":
-            filename = "__"
-        if len(ext) > 254:
-            ext = ext[254:]
-        maxl = 255 - len(ext)
-        filename = filename[:maxl]
-        filename = filename + ext
-        # Re-check last character (if there was no extension)
+def handle_reserved(filename: str) -> str:
+    """Handle reserved filenames."""
+    
+    # Reserved words on Windows
+    reserved = [
+        "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5",
+        "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5",
+        "LPT6", "LPT7", "LPT8", "LPT9",
+    ]  
+    return "__" + filename if filename in reserved else filename
+
+def handle_max_length(filename: str, ext: str, max_filename_length: int, 
+                        max_extension_length: int) -> str:
+    """Handle filenames that exceed the maximum length."""
+    if len(filename) > max_filename_length:
+        if len(ext) > max_extension_length:
+            ext = ext[max_extension_length:]
+        maxl = max_filename_length - len(ext)
+        filename = filename[:maxl] + ext
         filename = filename.rstrip(". ")
-        if len(filename) == 0:
-            filename = "__"
+        filename = "__" if len(filename) == 0 else filename
     return filename
