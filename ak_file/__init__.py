7c6e89e3d52a570d2e5d21bb38e4bfbfd74e0f2a
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2023-09-07 15:35:38 -0700

    obfuscate/unobfuscate using simple char shift

--- ak_file/__init__.py
+++ ak_file/__init__.py
@@ -1,7 +1,8 @@
 """ File Parsing function
 """
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from ak_file.main import File
 from ak_file.search import SearchFolder
 from ak_file.sanitizer import sanitize
+from ak_file import sanitizer
--- ak_file/sanitizer.py
+++ ak_file/sanitizer.py
@@ -1,6 +1,7 @@
 import unicodedata
 import re
 from pathlib import Path
+import string
 
 MAX_FILENAME_LENGTH: int = 255
 MAX_EXTENSION_LENGTH: int = 254
@@ -16,40 +17,40 @@ def sanitize(filename:str, strict: bool = True) -> str:
     else:
         filedir: Path = Path()
     
-    filename = remove_blacklisted_characters(filename)
-    filename = remove_chars_below_code_point(filename, MIN_ASCII_VALUE)
-    filename = normalize_filename(filename)
-    filename, ext = handle_extension(filename)
-    filename = handle_reserved(filename)
-    filename = handle_max_length(filename, ext, 
+    filename = _remove_blacklisted_characters(filename)
+    filename = _remove_chars_below_code_point(filename, MIN_ASCII_VALUE)
+    filename = _normalize_filename(filename)
+    filename, ext = _handle_extension(filename)
+    filename = _handle_reserved(filename)
+    filename = _handle_max_length(filename, ext, 
                                     MAX_FILENAME_LENGTH, MAX_EXTENSION_LENGTH)
 
     return str(filedir / filename)
 
 
-def remove_blacklisted_characters(filename: str) -> str:
+def _remove_blacklisted_characters(filename: str) -> str:
     """Remove blacklisted characters from the filename."""
     blacklist = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|", "\0"]
     return "".join(c for c in filename if c not in blacklist)
 
-def remove_chars_below_code_point(filename: str, code_point: int) -> str:
+def _remove_chars_below_code_point(filename: str, code_point: int) -> str:
     """Remove characters from the filename below a certain ASCII code point."""
     return "".join(c for c in filename if ord(c) > code_point)
 
-def normalize_filename(filename: str) -> str:
+def _normalize_filename(filename: str) -> str:
     """Normalize the filename and remove trailing/leading spaces and periods."""
     filename = unicodedata.normalize("NFKD", filename)
     filename = filename.rstrip(". ").strip()
     return "__" + filename if all([x == "." for x in filename]) else filename
 
-def handle_extension(filename: str) -> tuple[str, str]:
+def _handle_extension(filename: str) -> tuple[str, str]:
     """Handle the extension of the filename."""
     parts = re.split(r"/|\\", filename)[-1].split(".")
     ext = "." + parts.pop() if len(parts) > 1 else ""
     filename = filename[:-len(ext)] if ext else filename
     return filename, ext
 
-def handle_reserved(filename: str) -> str:
+def _handle_reserved(filename: str) -> str:
     """Handle reserved filenames."""
     
     # Reserved words on Windows
@@ -60,7 +61,7 @@ def handle_reserved(filename: str) -> str:
     ]  
     return "__" + filename if filename in reserved else filename
 
-def handle_max_length(filename: str, ext: str, max_filename_length: int, 
+def _handle_max_length(filename: str, ext: str, max_filename_length: int, 
                         max_extension_length: int) -> str:
     """Handle filenames that exceed the maximum length."""
     if len(filename) > max_filename_length:
@@ -71,3 +72,38 @@ def handle_max_length(filename: str, ext: str, max_filename_length: int,
         filename = filename.rstrip(". ")
         filename = "__" if len(filename) == 0 else filename
     return filename
+
+def _stringshift(stringlist: str, shift: int) -> str:
+    return stringlist[shift:] + stringlist[:shift]
+    
+def obfuscate(text: str, shift_val: int = 17) -> str:
+    stringlist: str = f"{string.ascii_letters}0123456789. "
+    
+    replacement_dict: dict[str, str] = {}
+    for original, replaced in zip(stringlist, _stringshift(stringlist, shift_val)):
+        replacement_dict[original] = replaced
+    
+    new_text: str = ""
+    for char in text:
+        new_char = replacement_dict.get(char)
+        if new_char is None:
+            new_text += char
+        else:
+            new_text += new_char
+    return new_text
+
+def unobfuscate(text: str, shift_val: int = 17) -> str:
+    stringlist: str = f"{string.ascii_letters}0123456789. "
+    
+    replacement_dict: dict[str, str] = {}
+    for original, replaced in zip(stringlist, _stringshift(stringlist, shift_val)):
+        replacement_dict[replaced] = original
+    
+    new_text: str = ""
+    for char in text:
+        new_char = replacement_dict.get(char)
+        if new_char is None:
+            new_text += char
+        else:
+            new_text += new_char
+    return new_text
